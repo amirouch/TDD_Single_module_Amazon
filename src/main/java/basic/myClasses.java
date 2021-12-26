7fe1dab36321bdc344c0a81f21d8c6e521340273
amirouch <tech.tabti@gmail.com>
2021-12-26 02:17:48 -0500

    git ignore file

---
+++ src/main/java/basic/myClasses.java
@@ -0,0 +1,11 @@
+package basic;
+
+import org.testng.annotations.Test;
+
+public class myClasses {
+
+    public static void myMethod(){
+        System.out.println("this is my method");
+    }
+
+}
---
+++ src/test/java/basicTest/myClassesTest.java
@@ -0,0 +1,12 @@
+package basicTest;
+
+import basic.myClasses;
+import org.testng.annotations.Test;
+
+public class myClassesTest {
+
+    @Test
+    public void testMyMethod(){
+        myClasses.myMethod();
+    }
+}
